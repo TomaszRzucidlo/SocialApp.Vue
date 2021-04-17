8213a4dc4d8b311036d59de1bbf8563d077bae98
TomaszRzucidlo <rzucidlo.tomasz@gmail.com>
2021-04-17 22:28:02 +0200

    TASK-FirstConfiguration Congigure vuex and routes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/admin">Admin</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useStore } from 'vuex';
+import {computed} from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default ({
+  setup() {
+    const store = useStore();
+    const user = computed(() => store.state.user);
+
+    return{
+      store,
+      user
+    }
+  },
+})
 </script>
 
-<style>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Admin from '../views/Admin.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about/:id',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },{
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    meta:{
+      requiresAdmin: true
+    }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach(async (to, from, next) =>{
+  const isAdmin = true;
+  const requiresAdmin = to.matched.some(record => record.meta.requiresAdmin);
+
+  if(requiresAdmin && !isAdmin) next({name:"Home"})
+  else next();
+})
+
+export default router
